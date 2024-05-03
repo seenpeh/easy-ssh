2e4a15d126fbe249cdb7f87dc203f0868fc9a332
seenpeh <seenpeh@gmail.com>
2024-05-03 16:40:31 +0330

    added the option to remove

--- add.sh
+++ add.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 
-read -p "Enter the server ip: " ip
+# Function to add a new host to ~/.ssh/config
+add_host() {
+    read -p "Enter the server ip: " ip
 
+    read -p "Enter the server user (Enter for default 'root'): " user
 
-read -p "Enter the server user (Enter for default 'root'): " user
+    if [ -z "$user" ]; then
+      user="root"
+    fi
 
-if [ -z "$user" ]; then
-  user="root"
-fi
+    read -p "Enter the server name: (For example Germany1): " name
 
-read -p "Enter the server name: (For example Germany1): " name
+    echo "Host $name
+      HostName $ip
+      User $user" >> ~/.ssh/config
 
-echo "Host $name
-  HostName $ip
-  User $user" >> ~/.ssh/config
+    rsa_file="$HOME/.ssh/id_ed25519"
 
-rsa_file="$HOME/.ssh/id_ed25519"
+    if [ ! -f "$rsa_file" ]; then
+    ssh-keygen -t ed25519  
+    fi
 
-if [ ! -f "$rsa_file" ]; then
-ssh-keygen -t ed25519  
-fi
+    ssh-copy-id -i ~/.ssh/id_ed25519.pub $name
 
-ssh-copy-id -i ~/.ssh/id_ed25519.pub $name
+    echo "Server name: $name user/ip: $user@$ip" >> ~/.ssh/hosts
+    echo "Added host $name with IP $ip"
+}
 
-echo "done!"
+# Function to remove a host from ~/.ssh/config
+remove_host() {
+    cat ~/.ssh/hosts
+    read -p "Enter the server name to remove: " name
+    sed -i "/Host $name/,/^$/d" ~/.ssh/config
+    sed -i "/Host $name/,/^$/d" ~/.ssh/hosts
+    echo "Removed host $name"
+}
+
+# Main menu
+while true; do
+    echo "Select an option:"
+    echo "1. Add new host"
+    echo "2. Remove host"
+    echo "3. Exit"
+    read -p "Enter your choice: " choice
+    case $choice in
+        1) add_host ;;
+        2) remove_host ;;
+        3) exit ;;
+        *) echo "Invalid option";;
+    esac
+done
